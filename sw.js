766495a4b4c82e2a6ac6910fcf7bb7068c259ccb
Nikhil Khanna <khanna.nikhil@outlook.com>
2019-09-12 15:12:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,11 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open('thenikhilk').then(function (cache) {
             return cache.addAll([
-                '/'
+                '/index.html',
+                '/favicon.ico',
+                '/assets/css/main.css',
+                '/assets/css/font-awesome.min.css',
+                '/assets/css/images/bg.jpg',
             ]);
         })
     );
