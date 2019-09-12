e306f9b19122773dcf26202f916c0b38090b0b3e
Nikhil Khanna <khanna.nikhil@outlook.com>
2019-09-12 14:26:13 +0530

    added service worker

---
+++ assets/js/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('thenikhilk').then(function (cache) {
+            return cache.addAll([]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
